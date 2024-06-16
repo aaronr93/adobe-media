ec0e860675a53f64f0261ea8f3b17382701fbc42
arosenberger <arosenberger@silverchair.com>
2024-06-16 08:51:01 -0400

    add squidyavi.bat backup (located in ProgramData)

---
+++ squidyavi.bat
@@ -0,0 +1,4 @@
+cd\ 
+cd "C:\Program Files\Adobe\Adobe After Effects 2020\Support Files\Scripts\ScriptUI Panels\ProjectCuddlePhish\"
+GIFsquidLesinski.bat 24 1500 "F:\Gif Fun 2\adobe-media\Projects\After Effects\Parks and Recreation\BAs are busy Parks and Recreation s02e12_1.avi" "F:\Gif Fun 2\adobe-media\Projects\After Effects\Parks and Recreation\CuddlePhish_Output" 100 1280:-1 "BAs are busy Parks and Recreation s02e12_1.avi" false
+exit
